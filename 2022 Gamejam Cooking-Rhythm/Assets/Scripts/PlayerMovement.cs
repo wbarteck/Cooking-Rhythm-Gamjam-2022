0a9d497df031463b5208f85dbe03b91a2c881aad
brnielsen <47045697+brnielsen@users.noreply.github.com>
2022-01-15 11:34:55 -0500

    Navmesh finds closest "grid point" and moves to it

--- 2022 Gamejam Cooking-Rhythm/Assets/Scripts/PlayerMovement.cs
+++ 2022 Gamejam Cooking-Rhythm/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     public Camera camera;  //TODO fix to work with cinemachine
 
     public NavMeshAgent agent;
+
+    public GridPoint[] allPoints;
    
     void Update()
     {
@@ -19,9 +21,25 @@ public class PlayerMovement : MonoBehaviour
 
             if(Physics.Raycast(ray, out raycastHit))
             {
-                agent.SetDestination(raycastHit.point); //TODO realign with created "grid" system
+                //Find nearest Grid Point
+                agent.SetDestination(FindNearestGridPoint(raycastHit.point)); //TODO realign with created "grid" system
             }
 
         }
+    }
+
+    Vector3 FindNearestGridPoint(Vector3 mousePoint)
+    {
+        Vector3 gridPoint = Vector3.positiveInfinity;
+        for (int i = 0; i < allPoints.Length; i++)
+        {
+            if (Vector3.Distance(mousePoint, allPoints[i].transform.position) < Vector3.Distance(mousePoint, gridPoint))
+            {
+                gridPoint = allPoints[i].transform.position;
+            }
+
+        }
+
+        return gridPoint;
     }
 }
