b8d88feb44d00118e5f8f13e9b500f9b2aa6a2a3
wbarteck <wbarteck@gmail.com>
2022-01-21 19:29:55 -0500

    score fix

--- 2022 Gamejam Cooking-Rhythm/Assets/Scripts/Scoring/TrackComparison.cs
+++ 2022 Gamejam Cooking-Rhythm/Assets/Scripts/Scoring/TrackComparison.cs
@@ -8,34 +8,37 @@ public class TrackComparison : MonoBehaviour
 {
     public TMP_Text scoreText;
 
-    float tolerance = .1f; //notes within X second count as perfect timing
+    float tolerance = .01f; //notes within X second count as perfect timing
 
     public GameEvent goodScore;
     public GameEvent medScore;
     public GameEvent badScore;
 
-    public float badScoreMin = 10.1f;
-    public float badScoreMax = 20.0f;
-    public float medScoreMin = 10.1f;
-    public float medScoreMax = 4.9f;
-    public float goodScoreMin = 0.1f;
-    public float goodScoreMax = 5.0f;
+    public float badScoreStartPercent = 0.5f; // anything above 50% distance is bad
+    public float medScoreStartPercent = 0.25f;// anything between 25% an 50% is medium
+    public float goodScoreStartPercent = 0f;    // anything else is good
+
     public void Score()
     {
         Melody customerOrder = GameManager.instance.currentOrder;
         Track[] playerTracks = FindObjectsOfType<Station>(true).Select(s => s.track).ToArray();
 
         float distance = 0f;
+        int totalNotes = 0;
         foreach(Track t in customerOrder.tracks)
         {
             if (t.backgroundTrack) continue;
             // find matching player track
+            totalNotes += t.beats.Count;
             var playerTrack = playerTracks.Where(track => track.note == t.note).First();
             distance += CompareTracks(t, playerTrack);
         }
         Debug.Log($"Total Distance: {distance}");
-        scoreText.text = $"Higher numer = worse performance: {distance}";
-        CompareScore(distance);
+        var scorePercent = distance / totalNotes;
+        Debug.Log($"Score Percent: {scorePercent}");
+        scoreText.text = $"Yout Got {Mathf.Round((1f - scorePercent) * 100)}% of the notes correct";
+
+        CompareScore(scorePercent);
     }
 
     public float CompareTracks(Track a, Track b)
@@ -47,27 +50,28 @@ public class TrackComparison : MonoBehaviour
             var distanceList = a.beats.Select(x => Mathf.Abs(x - n));
             var closestNote = distanceList.Min();
 
-            if (closestNote > tolerance) distance += (closestNote - tolerance);
+            if (closestNote > tolerance) distance += Mathf.Clamp01(closestNote - tolerance);
 
         }
-        return (noteDifference * 1f) + distance;
+        return (noteDifference) + distance;
     }
 
-    public void CompareScore(float score)
+    public void CompareScore(float scorePercent)
     {
 
-        if (score >= badScoreMin)
+        if (scorePercent >= badScoreStartPercent)
         {
+            Debug.Log("Bad Score");
             badScore.Raise();
         }
-
-        if (score >= medScoreMax && score <= medScoreMin)
+        else if (scorePercent >= medScoreStartPercent)
         {
+            Debug.Log("Medium Score");
             medScore.Raise();
         }
-
-        if (score >= goodScoreMin && score <= goodScoreMax)
+        else
         {
+            Debug.Log("Good Score");
             goodScore.Raise();
         }
     }
